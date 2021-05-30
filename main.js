7bec940166175f9a7db7aed8e74a51a4a17be840
Bhagatameya21 <68258737+Bhagatameya21@users.noreply.github.com>
2021-05-30 13:53:14 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+     
+  // client id of the project
+
+  var clientId = "249743537309-kbs3vvok4j0dchbnhbfet943rk00lqbf.apps.googleusercontent.com";
+
+  // redirect_uri of the project
+
+  var redirect_uri = "http://127.0.0.1:5500/redirect.html";
+
+  // scope of the project
+
+  var scope = "https://www.googleapis.com/auth/youtube";
+
+  // the url to which the user is redirected to
+
+  var url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+  +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+  +"&access_type=offline";
+
+
+  // this is event click listener for the button
+
+  $("#login").click(function(){
+
+     // this is the method which will be invoked it takes four parameters
+
+     signIn(clientId,redirect_uri,scope,url);
+
+  });
+
+  function signIn(clientId,redirect_uri,scope,url){
+   
+     // the actual url to which the user is redirected to 
+
+     url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+     +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+     +"&access_type=offline";
+
+     // this line makes the user redirected to the url
+
+     window.location = url;
+
+
+
+
+  }
+
+
+
+});
---
+++ redirect.js
@@ -0,0 +1,322 @@
+$(document).ready(function(){
+    
+ 
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  const redirect_uri = "http://127.0.0.1:5500/redirect.html" // redirect_uri;
+  const client_secret = "PxnJ02FY49rxuT4Uvnly6AKt"; // client secret
+  const scope = "https://www.googleapis.com/auth/youtube";
+  var client_id = "249743537309-kbs3vvok4j0dchbnhbfet943rk00lqbf.apps.googleusercontent.com";// client id
+  var playlist;
+  var channelId;
+  var username;
+  var search;
+  var playlistId;
+  var API_KEY = "AIzaSyBSwCWo8iOzZE--zk3LJAZR4IwY53T5CcY";
+
+  $("#myplaylist").hide();
+
+  $("#myplaylist").click(function(){
+     
+      empty();
+
+      getMyPlaylists();
+
+  });
+
+  $("#buttonid").click(function(){
+
+      $("#myplaylist").show();
+
+      empty();
+
+      channelId = $("#channelId").val();
+
+      getChannelPlaylist(channelId);
+
+
+  });
+
+  $("#usernamebutton").click(function(){
+
+      $("#myplaylist").show();
+      
+      empty();
+
+      username = $("#usernamefield").val();
+
+      getChannelPlaylistByUserName(username);
+
+
+  });
+
+  $("#searchbutton").click(function(){
+
+      $("#myplaylist").show();
+
+      empty();
+
+      search = $("#search").val();
+
+      getChannelPlaylistBySearch(search);
+
+  });
+
+
+  $.ajax({
+      type: 'POST',
+      url: "https://www.googleapis.com/oauth2/v4/token",
+      data: {code:code
+          ,redirect_uri:redirect_uri,
+          client_secret:client_secret,
+      client_id:client_id,
+      scope:scope,
+      grant_type:"authorization_code"},
+      dataType: "json",
+      success: function(resultData) {
+         
+          
+         localStorage.setItem("accessToken",resultData.access_token);
+         localStorage.setItem("refreshToken",resultData.refreshToken);
+         localStorage.setItem("expires_in",resultData.expires_in);
+         //window.history.pushState({}, document.title, "/GitLoginApp/" + "upload.html");
+         window.history.replaceState({}, document.title, "http://127.0.0.1:5500/redirect.html");
+         
+         
+         
+         
+      }
+});
+
+  getMyPlaylists();
+
+  function stripQueryStringAndHashFromPath(url) {
+      return url.split("?")[0].split("#")[0];
+  }   
+     
+  
+  function getMyPlaylists()
+
+  {
+  
+      $.ajax({
+          type: "GET",
+          beforeSend: function(request) {
+              request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+              
+          },
+          url:"https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true&maxResults=25&key="+API_KEY+"&access_token=Bearer"+ " "+localStorage.getItem("accessToken"),
+
+          success: function (data) {
+
+              console.log(data);
+
+              //nextPageToken = data.nextPageToken;
+
+
+
+              //$("#results").append(data.items.snippet.channelTitle
+             
+              data.items.forEach(item => {
+                 playlist = `
+                 <div style="text-align:center;">     
+                      <li>
+                      <h1>${item.snippet.title}</h1>
+                      <img src="${item.snippet.thumbnails.medium.url}" class="img-rounded">
+                      <h3>${item.snippet.description}</h3>
+                      <a href="https://www.youtube.com/playlist?list=${item.id}" target="_blank">Go To Playlist</a>
+                      </li>
+                 </div>     
+                      `;
+                 $("#results1").append(playlist); 
+              });
+
+          
+
+          },
+          error: function (error) {
+              console.log(error);
+          },
+          
+      });
+  
+  }
+
+
+
+
+  function getChannelPlaylist(channelId)
+  {
+
+      $.ajax({
+          type: "GET",
+          beforeSend: function(request) {
+              request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+              
+          },
+          url: "https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=25&channelId="+channelId+"&access_token=Bearer"+ " "+localStorage.getItem("accessToken"),
+
+          success: function (data) {
+
+              console.log(data);
+
+              //$("#results").append(data.items.snippet.channelTitle);
+             
+              data.items.forEach(item => {
+                 playlist = `
+                 <div style="text-align:center;">     
+                      <li>
+                      <h1>${item.snippet.title}</h1>
+                      <img src="${item.snippet.thumbnails.medium.url}" class="img-rounded">
+                      <h3>${item.snippet.description}</h3>
+                      <a href="https://www.youtube.com/playlist?list=${item.id}" target="_blank">Go To Playlist</a>
+                      </li>
+                 </div>     
+                      `;
+                 $("#results2").append(playlist); 
+              });
+
+
+          },
+          error: function (error) {
+              console.log(error);
+          },
+          
+      });
+  
+
+  }
+
+  function getChannelPlaylistByUserName(username)
+  {
+      $.ajax({
+          type: "GET",
+          beforeSend: function(request) {
+              request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+              
+          },
+          url: "https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails"+"&maxResults=25&forUsername="+username+"&access_token=Bearer"+ " "+localStorage.getItem("accessToken"),
+
+          success: function (data) {
+
+              console.log(data);
+
+              channelId = data.items[0].id;
+
+              getChannelPlaylistIdUserName(channelId);
+
+              //$("#results").append(data.items.snippet.channelTitle);
+
+          },
+          error: function (error) {
+              console.log(error);
+          },
+          
+      });
+  
+  }
+
+  function getChannelPlaylistBySearch(search)
+
+  {
+
+      $.ajax({
+          type: "GET",
+
+          beforeSend: function(request) {
+              request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+              
+          },
+          
+          url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+search+"&type=playlist",
+
+          success: function (data) {
+
+              console.log(data);
+
+              data.items.forEach(item => {
+                 
+                  playlistId = item.id.playlistId;
+
+                  playlist = `
+                 <div style="text-align:center;">     
+                      <li>
+                      <h1>${item.snippet.title}</h1>
+                      <img src="${item.snippet.thumbnails.medium.url}" class="img-rounded">
+                      <h3>${item.snippet.description}</h3>
+                      <a href="https://www.youtube.com/playlist?list={playlistId}" target="_blank">Go To Playlist</a>
+                      </li>
+                 </div>     
+                      `;
+
+                 $("#results4").append(playlist);
+
+                  
+                  
+              });
+
+              //$("#results").append(data.items.snippet.channelTitle);
+
+          },
+          error: function (error) {
+              console.log(error);
+          },
+          
+      });
+
+
+  }
+
+  function getChannelPlaylistIdUserName(channelId)
+
+  {
+      $.ajax({
+          type: "GET",
+          beforeSend: function(request) {
+              request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+              
+          },
+          url: "https://www.googleapis.com/youtube/v3/playlists?part=snippet&maxResults=25&channelId="+channelId+"&access_token=Bearer"+ " "+localStorage.getItem("accessToken"),
+
+          success: function (data) {
+
+              console.log(data);
+
+              //$("#results").append(data.items.snippet.channelTitle);
+             
+              data.items.forEach(item => {
+                 playlist = `
+                 <div style="text-align:center;">     
+                      <li>
+                      <h1>${item.snippet.title}</h1>
+                      <img src="${item.snippet.thumbnails.medium.url}" class="img-rounded">
+                      <h3>${item.snippet.description}</h3>
+                      <a href="https://www.youtube.com/playlist?list=${item.id}" target="_blank">Go To Playlist</a>
+                      </li>
+                 </div>     
+                      `;
+                 $("#results3").append(playlist); 
+              });
+
+
+          },
+          error: function (error) {
+              console.log(error);
+          },
+          
+      });
+  
+
+  }
+
+  function empty()
+  {
+      $("#results1").empty();
+      $("#results2").empty();
+      $("#results3").empty();
+      $("#results4").empty();
+  }
+
+
+  
+})
